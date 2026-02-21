82a87af27c490b570dea5dfef7bf654af299e9ad
Asbarn <Yaroslavisgood@gmail.com>
2026-02-21 23:32:33 +0100

    init: initial project scaffold from cargo new

---
+++ target/debug/deps/prediction-ae22d3c9691250f8.d
@@ -0,0 +1,5 @@
+D:\Programming\Rust\prediction\target\debug\deps\prediction-ae22d3c9691250f8.d: src\main.rs
+
+D:\Programming\Rust\prediction\target\debug\deps\libprediction-ae22d3c9691250f8.rmeta: src\main.rs
+
+src\main.rs:
