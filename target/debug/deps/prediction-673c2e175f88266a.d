e0abf75db45935fc2d3a6c55ae15a40caa1c57a7
Asbarn <Yaroslavisgood@gmail.com>
2026-02-21 23:33:04 +0100

    chore: add .gitignore and remove tracked target directory

--- target/debug/deps/prediction-673c2e175f88266a.d
+++
@@ -1,5 +0,0 @@
-D:\Programming\Rust\prediction\target\debug\deps\prediction-673c2e175f88266a.d: src\main.rs
-
-D:\Programming\Rust\prediction\target\debug\deps\libprediction-673c2e175f88266a.rmeta: src\main.rs
-
-src\main.rs:
